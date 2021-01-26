a803db1a3686a5623f6dcaa98d5d080947c37f44
Miguel Peixe <miguel@peixe.co>
2021-01-26 13:11:31 -0300

    validate server url

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import { iframeResize } from "iframe-resizer";
 
 (function () {
+  function validateURL(string, allowNonSecure) {
+    let url;
+    try {
+      url = new URL(string);
+    } catch (err) {
+      return false;
+    }
+    if (allowNonSecure) {
+      return url.protocol === "http:" || url.protocol === "https:";
+    }
+    return url.protocol === "https:";
+  }
   window.onload = function () {
     const nodes = document.getElementsByClassName("liane-form");
     for (const container of nodes) {
-      const iframe = document.createElement("iframe");
       const server = container.getAttribute("data-url");
       const campaignId = container.getAttribute("data-campaignId");
-      const url = `${server}/f/?c=${campaignId}`;
-      container.appendChild(iframe);
-      iframe.setAttribute("style", `min-width: 400px; width: 100%;`);
-      iframe.setAttribute("src", url);
-      iframe.setAttribute("frameborder", 0);
-      iframeResize({}, iframe);
+      const nonSecure = !!container.getAttribute("data-allowNonSecure");
+      if (validateURL(server, nonSecure)) {
+        const iframe = document.createElement("iframe");
+        const url = `${server}/f/?c=${campaignId}`;
+        container.appendChild(iframe);
+        iframe.setAttribute("style", `min-width: 400px; width: 100%;`);
+        iframe.setAttribute("src", url);
+        iframe.setAttribute("frameborder", 0);
+        iframeResize({}, iframe);
+      }
     }
   };
 })();
