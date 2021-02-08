1c45a7a61202aa97b424803941ed7f8c64e184bd
Miguel Peixe <miguel@peixe.co>
2021-02-08 15:52:49 -0300

    support compact mode and update min-width

--- src/index.js
+++ src/index.js
@@ -13,17 +13,32 @@ import { iframeResize } from "iframe-resizer";
     }
     return url.protocol === "https:";
   }
+  function serialize(obj) {
+    const str = [];
+    for (const p in obj)
+      if (obj.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      }
+    return str.join("&");
+  }
   window.onload = function () {
     const nodes = document.getElementsByClassName("liane-form");
     for (const container of nodes) {
       const server = container.getAttribute("data-url");
       const campaignId = container.getAttribute("data-campaignId");
+      const compact = container.getAttribute("data-compact");
       const nonSecure = !!container.getAttribute("data-allowNonSecure");
+      const params = {
+        c: campaignId,
+      };
+      if (compact) {
+        params.compact = true;
+      }
       if (validateURL(server, nonSecure)) {
         const iframe = document.createElement("iframe");
-        const url = `${server.replace(/\/$/, "")}/f/?c=${campaignId}`;
+        const url = `${server.replace(/\/$/, "")}/f/?${serialize(params)}`;
         container.appendChild(iframe);
-        iframe.setAttribute("style", `min-width: 400px; width: 100%;`);
+        iframe.setAttribute("style", `min-width: 200px; width: 100%;`);
         iframe.setAttribute("src", url);
         iframe.setAttribute("frameborder", 0);
         iframeResize(
